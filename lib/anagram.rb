4c52a9887fd6863c46955bbe51112be53c67d3bd
jkhoang313 <jkhoang313@gmail.com>
2016-11-30 12:06:29 -0500

    passed tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_matches)
+    possible_matches.select {|anagram| @word.split("").sort == anagram.split("").sort}
+  end
+end
